db3418402ae406410c961f5625f29f1b8ca88416
CastleStar14654 <CastleStar14654@Gmail.com>
2020-06-06 14:23:52 +0800

    final version with comment

--- restart.py
+++ restart.py
@@ -4,6 +4,7 @@ evaluate(board, isFirst) 返回 isFirst 方与 not isFirst 方的局面之差
 import random
 import operator
 
+# 常量定义
 POSITION_MODE = 'position'
 DIRECTION_MODE = 'direction'
 _POSITION_MODE = '_position'
@@ -13,20 +14,31 @@ UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
 
 INF = float('inf')
 
-MERGE_INTERFERENCE_THRESHOLD = 4 # 仅处理 2^5 及以上的合并
-BASE_POWER = 2 # 3
-ENEMY_WEIGHT = 1.25 # 1
-BONUS_POWER = BASE_POWER
-ENEMY_BONUS_POWER = BASE_POWER - 1
+MERGE_INTERFERENCE_THRESHOLD = 4  # 仅处理 2^5 及以上的合并
+BASE_POWER = 2  # 基础分值为 2**(BASE_POWER * 级别)
+ENEMY_WEIGHT = 0.75  # 估值为己方分值减去 ENEMY_WEIGHT*对方分值
+BONUS_POWER = BASE_POWER  # 己方棋子在对方领域接近己方的两列时，有额外加分 2**(BONUS_POWER * 级别)
+FAR_BONUS_POWER = BASE_POWER - 1 # 己方棋子在对方领域远离己方的两列时，有额外加分 2**(FAR_BONUS_POWER * 级别)
 
 
 class Node:
+    '''搜索树的节点.
+    使用 self.node: dict 作为数据结构，每个节点存储
+        self.isFirst: 此树是否在先手的视角下生成
+        self.mode: 此节点决策模式. 可为 `POSITION` 或 `DIRECTION`
+        self.board: 待决策棋盘
+        self.currentRound: 本节点轮数
+        self.evaluate: 估值函数
+        self.minimax: bool, 是否为 min 节点
+        self.alpha, self.beta: ab剪枝用参数
+        self.nodes: 节点
+    '''
+    # 对对方合并方向的计数
     direction_count = {True: {RIGHT: 3, UP: 1, DOWN: 1, LEFT: 0},
                        False: {LEFT: 3, UP: 1, DOWN: 1, RIGHT: 0}}
 
     def __init__(self, isFirst: bool, mode: str, board, currentRound: int,
-                 evaluate, minimax: bool, alpha=-INF,
-                 beta: float = INF, depth: int = None):
+                 evaluate, minimax: bool, alpha=-INF, beta: float = INF):
         '''生成节点。
         player: 玩家
         isFirst: 这棵 ***树*** 是 !!!以谁的视角!!! 生成的
@@ -37,7 +49,6 @@ class Node:
         evaluate: 估值函数, evaluate(board, isFirst) 返回 isFirst 方与 not isFirst 方的局面之差
         minimax: min `True`, max `False`
         alpha, beta: alpha, beta值
-        depth: 如果是根节点, 传入期待这棵树有多深. 根节点深度记为 0
         '''
         self.isFirst = isFirst
         self.mode = mode
@@ -48,7 +59,6 @@ class Node:
         self.minimax = minimax
         self.alpha = alpha
         self.beta = beta
-        self.depth = depth
         self.nodes = {}  # {operation: Node}
 
         # `ab_attr` 归本层管的参数
@@ -64,8 +74,8 @@ class Node:
             self.direction_count[not self.isFirst][operation] += 1
 
     def deepen(self, modes: list):
-        '''加深并搜索树. modes 为新的层的 mode
-        返回 新的 alpha 或 beta
+        '''加深并搜索树. modes 为由新的层的 (mode, round) 元组构成的 list
+        返回本层新的 alpha 或 beta
         '''
         if modes:
             # 本层没被搜过, 而且还要往更深层搜
@@ -80,7 +90,7 @@ class Node:
                         continue
                     # 创建新节点, 给下一层
                     self.nodes[op_func[2]] = Node(self.isFirst, modes[0][0], board_copy, modes[0][1],
-                                                  self.evaluate, not self.minimax, -INF, INF, None)
+                                                  self.evaluate, not self.minimax, -INF, INF)
                     # 下面与之前类似
                     setattr(self.nodes[op_func[2]],
                             self.ab_attr, getattr(self, self.ab_attr))
@@ -93,7 +103,7 @@ class Node:
             if not self.nodes:
                 # 如果前面没搜出什么东西, 那么把空操作弄进去
                 self.nodes[()] = Node(self.isFirst, modes[0][0], self.board.copy(), modes[0][1],
-                                      self.evaluate, not self.minimax, -INF, INF, None)
+                                      self.evaluate, not self.minimax, -INF, INF)
                 setattr(self.nodes[()], self.ab_attr,
                         getattr(self, self.ab_attr))
                 new_value = self.nodes[()].deepen(modes[1:])
@@ -120,7 +130,6 @@ class Node:
         '''
         for op, node in self.nodes.items():
             if node.beta >= self.alpha:
-                node.depth = self.depth - 1
                 return op, node
         raise RuntimeError('no decision')
 
@@ -134,36 +143,31 @@ class Node:
                 pos = self.board.getNext(
                     self.isFirst ^ self.minimax, self.currentRound)
             except IndexError:
+                # 回合数到头
                 pos = ()
             if pos:
                 # 如果可以在自己这边放
                 res.append(('add', self.isFirst ^ self.minimax, pos))
 
             if not res:
-                # 如果局势比较晚, 或者不可以在自己这里放, 那么随机搞两个
-                '''TODO: 更好的获得位置的方法
-                '''
+                # 如果局势比较晚, 或者不可以在自己这里放, 那么随机搞一个
                 nones = self.board.getNone(self.isFirst == self.minimax)
                 if nones:
                     random.shuffle(nones)
                     res.append(('add', self.isFirst == self.minimax, nones[0]))
-
         else:
-            '''TODO 对方进攻/防守策略判断及优化'''
             if self.minimax:
-                # 对方
+                # 对方，根据对方已有决策进行猜测
                 d = self.direction_count[not self.isFirst]
                 d = sorted(d, key=lambda x: d[x], reverse=True)
             else:
+                # 己方，优先进攻
                 d = (RIGHT, UP, DOWN, LEFT) \
                     if self.isFirst \
                     else (LEFT, UP, DOWN, RIGHT)
             for direction in d:
                 res.append(('move', self.isFirst ^ self.minimax, direction))
         return res
-
-    def __repr__(self):
-        return repr(self.mode) + repr(self.nodes)
 
 
 def find_pos(board_raw: list, to_belong: bool, direction: int) -> tuple:
@@ -208,14 +212,17 @@ def find_pos(board_raw: list, to_belong: bool, direction: int) -> tuple:
 
 
 class Player:
+    '''任务要求的 `Player` 接口
+    self.tree: 搜索树的根节点
+    '''
     def __init__(self, isFirst: bool, array: list) -> None:
         self._isFirst = isFirst
         self._array = array
         self.tree = Node(isFirst, POSITION_MODE, None, 0,
-                         self.evaluate, False, depth=0)
+                         self.evaluate, False)
 
     def output(self, currentRound: int, board, mode: str):
-        # 获得上部操作, 记进小本本
+        # 获得上步操作, 记进小本本
         prev_decision = board.getDecision(not self._isFirst)
         self.tree.count_direction(prev_decision)
 
@@ -226,7 +233,7 @@ class Player:
         # 建设搜索树
         decision = None
         self.tree = Node(self._isFirst, mode, board.copy(), currentRound,
-                         self.evaluate, False, depth=0)
+                         self.evaluate, False)
         if mode == POSITION_MODE:
             # 尝试看有没有高收益的阻碍对方合并的地方
             # 寻找对面的大子合并方向
@@ -294,12 +301,13 @@ class Player:
         return decision
 
     def evaluate(self, board, isFirst: bool):
-        '''估值函数 TODO 返回 isFirst 方与 not isFirst 方的局面之差
-        TODO 先后手是否可以两个版本?
+        '''估值函数返回 isFirst 方与 not isFirst 方的局面之差
         '''
+        # 基本分
         def func(x): return 2 ** (BASE_POWER * x)
         res = sum(map(func, board.getScore(isFirst))) - \
             ENEMY_WEIGHT * sum(map(func, board.getScore(not isFirst)))
+        # 在对面的棋子的附加分
         boardList = board.getRaw()
         for col in range(4, 6) if isFirst else range(2, 4):
             for row in range(4):
@@ -308,5 +316,5 @@ class Player:
         for col in range(6, 8) if isFirst else range(2):
             for row in range(4):
                 if boardList[row][col][1] == isFirst:
-                    res += 2 ** (ENEMY_BONUS_POWER * boardList[row][col][0])
+                    res += 2 ** (FAR_BONUS_POWER * boardList[row][col][0])
         return res
